fc48b28824147793d0f9ba8a00e5401674ff4893
App Academy Student <email@email.com>
2022-03-15 16:26:36 -0400

    working on albums

---
+++ music_app/app/models/album.rb
@@ -0,0 +1,9 @@
+class Album < ApplicationRecord
+
+    validates, :title, :band_id, :year, :live, presence: true
+
+    belongs_to :band,
+        primary_key: :id,
+        foreign_key: :band_id,
+        class_name: :Band
+end
---
+++ music_app/app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ApplicationRecord
+
+    after_initialize :ensure_session_token
+
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true}
+
+    attr_reader :password
+
+    def self.generate_session_token
+        session_token = SecureRandom.urlsafe_base64
+    end
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password
+        puts 'in setter'
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def password
+        @password
+        puts 'in getter'
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+end
